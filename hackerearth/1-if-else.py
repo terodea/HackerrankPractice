3123e937439c9dcb412ef2abd1b805064b2f2154
Akshay Terode <terodeakshay@gmail.com>
2018-07-05 01:21:50 +0530

    Create 1-if-else.py

---
+++ hackerearth/1-if-else.py
@@ -0,0 +1,17 @@
+'''Task 
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird '''
+
+n = int(input())
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
