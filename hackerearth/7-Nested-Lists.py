b334d8fa99a79c109134261f8746ba6cc8cf6cd5
Akshay Terode <terodeakshay@gmail.com>
2018-07-13 13:32:41 +0530

    Update 7-Nested-Lists.py

--- hackerearth/7-Nested-Lists.py
+++ hackerearth/7-Nested-Lists.py
@@ -1,4 +1,7 @@
 """
+Given the names and grades for each student in a Physics class of N students, 
+store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+If there are multiple students with the same grade, order their names alphabetically and print each name on a new line.
 """
 #Read input and append the list to main records list using List Compression
 records = [[input(),float(input())] for i in range(int(input()))]
