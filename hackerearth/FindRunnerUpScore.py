64ff0a12abee0c4cdc896042aae6a7339285c788
Akshay Terode <terodeakshay@gmail.com>
2018-07-16 18:53:25 +0530

    Create FindRunnerUpScore.py

---
+++ hackerearth/FindRunnerUpScore.py
@@ -0,0 +1,5 @@
+"""
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+You are given "N" scores. Store them in a list and find the score of the runner-up.
+"""
+print raw_input()==0 or sorted(set(map(int, raw_input().split())))[-2]
