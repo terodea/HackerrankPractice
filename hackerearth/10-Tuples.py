2a6aaaff7456eae6ca5d33db476af7c6866625d6
Akshay Terode <terodeakshay@gmail.com>
2018-07-16 18:27:11 +0530

    Update 10-Tuples.py

--- hackerearth/10-Tuples.py
+++ hackerearth/10-Tuples.py
@@ -1,3 +1,9 @@
+"""
+Compute hash(): 
+I/p : 1st Line - Tuple size
+      2nd Line - Tuple members.
+O/p : Hash()
+"""
 if __name__ == '__main__':
     n = int(raw_input())
     integer_list = tuple(map(int, raw_input().split()))
