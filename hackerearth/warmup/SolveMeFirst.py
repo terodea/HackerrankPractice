c79e6c8445eb0a8049c2813eca3efb3e5d4952bc
Akshay Terode <terodeakshay@gmail.com>
2018-07-17 14:05:45 +0530

    Create SolveMeFirst.py

---
+++ hackerearth/warmup/SolveMeFirst.py
@@ -0,0 +1,11 @@
+"""
+Complete the function solveMeFirst to compute the sum of two integers.
+"""
+def solveMeFirst(a,b):
+	return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
