26bb590961aca475c22567ae36e4476ae230e63c
Akshay Terode <terodeakshay@gmail.com>
2018-07-22 02:05:04 +0530

    Create MergeTools.py

---
+++ hackerearth/python/strings/MergeTools.py
@@ -0,0 +1,11 @@
+"""
+Given String and K , print n/k  lines where each line  'i'  denotes string 'ui'.
+"""
+def merge_the_tools(string, k):
+    for part in zip(*[iter(string)] * k):
+        d = dict()
+        print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
+        
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
