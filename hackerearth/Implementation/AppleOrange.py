e2a82974298c5fd0c4ebce6230b8abd8840cd6f2
Akshay Terode <terodeakshay@gmail.com>
2018-07-25 13:02:31 +0530

    Create AppleOrange.py

---
+++ hackerearth/Implementation/AppleOrange.py
@@ -0,0 +1,26 @@
+"""
+https://www.hackerrank.com/challenges/apple-and-orange/problem
+"""
+import math
+import os
+import random
+import re
+import sys
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    print(sum([s <= a + d <= t for d in apples]))
+    print(sum([s <= b + d <= t for d in oranges]))
+
+if __name__ == '__main__':
+    st = input().split()
+    s = int(st[0])
+    t = int(st[1])
+    ab = input().split()
+    a = int(ab[0])
+    b = int(ab[1])
+    mn = input().split()
+    m = int(mn[0])
+    n = int(mn[1])
+    apples = list(map(int, input().rstrip().split()))
+    oranges = list(map(int, input().rstrip().split()))
+    countApplesAndOranges(s, t, a, b, apples, oranges)
