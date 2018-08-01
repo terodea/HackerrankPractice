f3479d5586c095877765288bac890145b6009c54
Akshay Terode <terodeakshay@gmail.com>
2018-08-01 12:49:07 +0530

    Create 8-CaptainsRoom.py

---
+++ hackerearth/python/sets/8-CaptainsRoom.py
@@ -0,0 +1,16 @@
+"""
+https://www.hackerrank.com/challenges/py-the-captains-room/problem
+"""
+k = int(input())
+arr = list(map(int, input().split()))
+st = set(arr)
+for x in st:
+    count = 0
+    for i in arr:
+        if x == i:
+            count += 1
+            if count > 1:
+                break
+    if count == 1:
+        print (x)
+        break
