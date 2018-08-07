f7314222fda5e63956157f53b0add1de71e29075
Akshay Terode <terodeakshay@gmail.com>
2018-08-07 21:29:39 +0530

    Create DivisibleSumPairs.py

---
+++ hackerearth/Implementation/DivisibleSumPairs.py
@@ -0,0 +1,19 @@
+"""
+"""
+import math
+import os
+import random
+import re
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    return sum(1 for i in range(n) for j in range(i+1 ,n) if (ar[i]+ar[j])%k == 0)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nk = input().split()
+    n = int(nk[0])
+    k = int(nk[1])
+    ar = list(map(int, input().rstrip().split()))
+    result = divisibleSumPairs(n, k, ar)
+    fptr.write(str(result) + '\n')
