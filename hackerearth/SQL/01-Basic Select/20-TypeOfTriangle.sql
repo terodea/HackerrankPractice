ababd884006328b7239d79d89253f4f22d8d1466
Akshay Terode <terodeakshay@gmail.com>
2018-08-28 22:23:51 +0530

    Create 20-TypeOfTriangle.sql

---
+++ hackerearth/SQL/01-Basic Select/20-TypeOfTriangle.sql
@@ -0,0 +1,8 @@
+SELECT 
+    CASE 
+        WHEN A >= (B + C) OR B >= (A + C) OR C >= (A + B) THEN 'Not A Triangle'
+        WHEN A = B AND A = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES;
